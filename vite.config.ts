5afd267ff6dae33182d1563d20b72973156856a5
James Ly <jamesquietly@gmail.com>
2025-10-18 20:06:20 -0700

    add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,30 @@ import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [sveltekit(), SvelteKitPWA()],
+  plugins: [
+    sveltekit(),
+    SvelteKitPWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Wordle",
+        short_name: "Wordle",
+        description:
+          "Wordle is a word game where players try to guess a hidden 5-letter word in 6 attempts. Each guess provides feedback on the position and presence of letters, helping players narrow down the possibilities and eventually solve the puzzle.",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/lib/assets/w-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/lib/assets/w-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
